a917b4a449bc48ef9ca339dc871dc495de4a5b8f
Sidney Mwangi <133115904+Kagwi09@users.noreply.github.com>
2025-01-06 11:47:11 +0300

    Add files via upload

--- Python/streamlit/main.py
+++ Python/streamlit/main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-
 from streamlit_option_menu import option_menu
 
 import about, account, home, trending, your_posts
@@ -11,36 +10,40 @@ st.set_page_config(
 class MultiApp:
     def __init__(self):
         self.apps = []
-    def add_app(self,title,function):
+    
+    def add_app(self, title, function):
         self.apps.append({
-            'title':title,
-            'function':function
+            'title': title,
+            'function': function
         })
-    def run():
+    
+    def run(self):  # Add self as the first argument
         with st.sidebar:
             app = option_menu(
                 menu_title='Pondering',
-                options=['Home','Account','Trending','Your Posts','About'],
-                icons =['house-fill','person-circle','trophy-fill','chat-fill','info-circle-fill'],
+                options=['Home', 'Account', 'Trending', 'Your Posts', 'About'],
+                icons=['house-fill', 'person-circle', 'trophy-fill', 'chat-fill', 'info-circle-fill'],
                 menu_icon='chat-text-fill',
                 default_index=1,
                 styles={
-                    'container':{'padding':'5!important','background-color':'black'},
-                    'icon':{'color':'white','font-size':'23px'},
-                    'nav-link':{'color':'white','font-size':'20px','text-align':'left','margin':'0px','--hover-color':'blue'},
-                    'nav-link-selected':{'background-color':'#02ab21'},
+                    'container': {'padding': '5!important', 'background-color': 'black'},
+                    'icon': {'color': 'white', 'font-size': '23px'},
+                    'nav-link': {'color': 'white', 'font-size': '20px', 'text-align': 'left', 'margin': '0px', '--hover-color': 'blue'},
+                    'nav-link-selected': {'background-color': '#02ab21'},
                 }
             )    
+        
         if app == 'Home':
             home.app()
-        if app == 'Account':
+        elif app == 'Account':
             account.app()
-        if app == 'Trending':
+        elif app == 'Trending':
             trending.app()
-        if app == 'Your Posts':
+        elif app == 'Your Posts':
             your_posts.app()
-        if app == 'About':
+        elif app == 'About':
             about.app()
-            
-            
-    run()
+
+# Create an instance of MultiApp and run it
+app = MultiApp()
+app.run()
