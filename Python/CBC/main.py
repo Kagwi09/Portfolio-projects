595731740efd79c4846661e60ab00128c14591b2
Sidney Mwangi <133115904+Kagwi09@users.noreply.github.com>
2025-01-22 10:38:57 +0300

    Update main.py

--- Python/CBC/main.py
+++ Python/CBC/main.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from PIL import Image
 from streamlit_option_menu import option_menu
-from CBC.project_gallery import run_project_gallery
-
+from project_gallery import run_project_gallery
 
 # Corrected image URL (direct raw link from GitHub)
 image_url = "https://raw.githubusercontent.com/Kagwi09/Portfolio-projects/main/Python/CBC/jamwi_logo.jpg"
